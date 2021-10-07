4d73e1bc616ba65710d3c52443bb0e4857aeaf42
Isaac Favila <imfavila@gmail.com>
2021-10-07 16:26:59 -0500

    add to testing suite

--- demo.js
+++ demo.js
@@ -1,7 +1,11 @@
 function sum(a, b) {
   return a + b;
 }
+function difference(a, b) {
+  return a - b;
+}
 
 module.exports = {
-  sum
+  sum,
+  difference,
 }
--- demo.test.js
+++ demo.test.js
@@ -2,7 +2,19 @@ const funcs = require('./demo');
 
 
 describe('Test sum function', () => {
-  test('adds 1 + 2 to equal 3', () => {
+  test('expect 1 + 2 to equal 3', () => {
     expect(funcs.sum(1, 2)).toEqual(3);
+  })
+  test('expect 1 + 2 to not equal 5', () => {
+    expect(funcs.sum(1, 2)).not.toEqual(5);
+  })
+});
+
+describe('Test difference function', () => {
+  test('expect 1 - 2 to equal -1', () => {
+    expect(funcs.difference(1, 2)).toEqual(-1);
+  })
+  test('expect 1 - 2 to not equal 0', () => {
+    expect(funcs.difference(1, 2)).not.toEqual(0);
   })
 });
