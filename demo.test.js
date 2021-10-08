9b2f0b609638ddb5312bce399ed8f5c0a5412022
Isaac Favila <imfavila@gmail.com>
2021-10-08 16:02:21 -0500

    include it

--- demo.test.js
+++ demo.test.js
@@ -5,7 +5,7 @@ describe('Test sum function', () => {
   test('expect 1 + 2 to equal 3', () => {
     expect(funcs.sum(1, 2)).toEqual(3);
   })
-  test('expect 1 + 2 to not equal 5', () => {
+  it('expect 1 + 2 to not equal 5', () => {
     expect(funcs.sum(1, 2)).not.toEqual(5);
   })
 });
@@ -14,7 +14,7 @@ describe('Test sum function', () => {
 //   test('expect Hello world to equal dlrow olleH', () => {
 //     expect(funcs.reverseString('Hello world')).toEqual('dlrow olleH');
 //   })
-//   test('expect result to be a string', () => {
+//   it('expect result to be a string', () => {
 //     expect(typeof funcs.reverseString('Hello')).toEqual('string');
 //   })
 // });
@@ -23,7 +23,7 @@ describe('Test sum function', () => {
 //   test('expect 1 to return ["R", "P", "S"]', () => {
 //     expect(funcs.rockPaperScissors(1)).toEqual(['R', 'P', 'S']);
 //   })
-//   test('expect 3 to have 27 combinations', () => {
+//   it('expect 3 to have 27 combinations', () => {
 //     expect(funcs.rockPaperScissors(3)).toHaveLength(27);
 //   })
 // });
@@ -32,7 +32,7 @@ describe('Test sum function', () => {
 //   test('expect RFE5 to return true', () => {
 //     expect(funcs.bestCohort('RFE5')).toBeTruthy();
 //   })
-//   test('expect anything else to return false', () => {
+//   it('expect anything else to return false', () => {
 //     expect(funcs.bestCohort('RFE6')).toBeFalsy();
 //   })
 // });
