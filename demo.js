b405718e4ea9a31d9086d50de437df06b9e1393e
Isaac Favila <imfavila@gmail.com>
2021-10-08 15:45:48 -0500

    Finsh test suite

--- demo.js
+++ demo.js
@@ -1,11 +1,33 @@
 function sum(a, b) {
   return a + b;
 }
-function difference(a, b) {
-  return a - b;
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
+function rockPaperScissors(rounds = 3) {
+  var rps = ['R', 'P', 'S'];
+  var combos = [];
+  function play(playedSoFar = '') {
+    if (playedSoFar.length === rounds) {
+      combos.push(playedSoFar);
+      return;
+    }
+    rps.forEach(function(currentRound) {
+      play(playedSoFar + currentRound);
+    });
+  };
+  play();
+  return combos;
+};
+
+function bestCohort(str) {
+  if (str === 'RFE5') return true;
+  return false;
+}
 module.exports = {
   sum,
-  difference,
+  reverseString,
+  rockPaperScissors,
+  bestCohort
 }
--- demo.test.js
+++ demo.test.js
@@ -10,11 +10,29 @@ describe('Test sum function', () => {
   })
 });
 
-describe('Test difference function', () => {
-  test('expect 1 - 2 to equal -1', () => {
-    expect(funcs.difference(1, 2)).toEqual(-1);
-  })
-  test('expect 1 - 2 to not equal 0', () => {
-    expect(funcs.difference(1, 2)).not.toEqual(0);
-  })
-});
+// describe('Test reverseString function', () => {
+//   test('expect Hello world to equal dlrow olleH', () => {
+//     expect(funcs.reverseString('Hello world')).toEqual('dlrow olleH');
+//   })
+//   test('expect result to be a string', () => {
+//     expect(typeof funcs.reverseString('Hello')).toEqual('string');
+//   })
+// });
+
+// describe('Test rockPaperScissors function', () => {
+//   test('expect 1 to return ["R", "P", "S"]', () => {
+//     expect(funcs.rockPaperScissors(1)).toEqual(['R', 'P', 'S']);
+//   })
+//   test('expect 3 to have 27 combinations', () => {
+//     expect(funcs.rockPaperScissors(3)).toHaveLength(27);
+//   })
+// });
+
+// describe('Test bestCohort function', () => {
+//   test('expect RFE5 to return true', () => {
+//     expect(funcs.bestCohort('RFE5')).toBeTruthy();
+//   })
+//   test('expect anything else to return false', () => {
+//     expect(funcs.bestCohort('RFE6')).toBeFalsy();
+//   })
+// });
